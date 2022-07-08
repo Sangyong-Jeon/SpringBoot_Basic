d54aac19ae1b8d9862431f1c4178826823159b2c
Sangyong-Jeon <tkddyd420@naver.com>
2022-07-08 13:58:46 +0900

    게시글 상세조회시 댓글 조회 JPQL 수정
    - where 조건문에 post를 안넣고 있던것을 수정

--- src/main/java/com/example/springboot_basic/repository/CommentRepository.java
+++ src/main/java/com/example/springboot_basic/repository/CommentRepository.java
@@ -4,12 +4,13 @@ import com.example.springboot_basic.domain.comment.Comment;
 import com.example.springboot_basic.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 
-    @Query("select c from Comment c where c.parent is null")
-    List<Comment> findCommentsForPost(Post post);
+    @Query("select c from Comment c where c.parent is null and c.post =:post")
+    List<Comment> findCommentsForPost(@Param("post") Post post);
 }
